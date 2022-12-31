c7ccdc9401c0270ad7bd48ed1a41f385520c510c
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-12-31 10:46:24 +0900

    feat: supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  srcDir: 'src/',
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+  }
 })
---
+++ src/pages/login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+const { auth } = useSupabaseClient()
+
+watchEffect(() => {
+  if (user.value) {
+    navigateTo('/tasks')
+  }
+})
+
+async function signInWithTwitter() {
+  const { data, error } = await auth.signInWithOAuth({
+    provider: 'twitter',
+  })
+}
+
+</script>
+
+
+<template>
+  <div class="button" @click="signInWithTwitter()">
+    ログイン
+  </div>
+</template>
+
+
+<style>
+.button {
+  width: 100px;
+  margin: 5px;
+  padding: 5px;
+  background-color: #cc66cc;
+  cursor: pointer;
+}
+</style>
