53e337196bace430aa32b6ae34d69a2d7fc71d8c
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-01-19 17:38:47 +0900

    update:

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   トップページ
+  <!-- <template name="forma"> -->
+    <!-- <FormKit label="Username" type="text" help="Pick a new username" validation="required|matches:/^@[a-zA-Z]+$/|length:5" value="@useFormKit" /> -->
+  <!-- </template> -->
 </template>
 
 <script setup>
 
 </script>
+
